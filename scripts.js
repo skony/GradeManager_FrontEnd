8b7d9245593c2a75788fed8f5a7cd242f2c58cb7
Piotr Skonieczny <piotr7skonieczny@gmail.com>
2016-06-08 17:36:44 +0200

    send ajax request on click

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
 "use strict";
 
-function MyObservableArray() {
-    
-}
+//function MyObservableArray() {
+//    
+//}
+//
+//var myArray = new MyObservableArray();
+//myArray.prototype = ko.observableArray();
 
-var myArray = new MyObservableArray();
-myArray.prototype = ko.observableArray();
+var studentListModel = function () {
+    this.items = ko.observableArray();
+    this.loadItems = function () {
+        $.ajax("http://localhost:8889/service/students", {
+            accepts: 'application/json',
+            type: 'GET',
+        })
+        .done(function (data) {
+            alert("SUCCESS");
+        })
+        .fail(function () {
+            alert("FAIL");
+        })
+    };
+};
 
+ko.applyBindings(new studentListModel());
